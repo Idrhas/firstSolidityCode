73cf63fb15cab0fce6edc9756dff5cfbb83fbdc5
Aboogeeky <Idrishassan35@gmail.com>
2024-03-05 04:32:04 +0100

    Update and rename SolidityAssignment.sol to Assignment.sol

--- Assignment.sol
+++ Assignment.sol
@@ -3,8 +3,8 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 /**
- * @title Storage
- * @dev Store & retrieve value in a variable
+ * @title Assigment
+ * @dev Store & retrieve values in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
 contract Assignment {
